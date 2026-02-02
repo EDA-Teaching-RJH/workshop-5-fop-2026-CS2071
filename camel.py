3aa1da68a8ccf47a7d373e6ec22a84e56a79bf81
CS2071 <cs2071@kent.ac.uk>
2026-02-02 14:41:29 +0000

    lists :(

--- camel.py
+++ camel.py
@@ -1,1 +1,9 @@
-# Enter your code here
+a = input("")
+i = 0
+Check = ["A", "B", "C"]
+Change = ["_a", "_b", "_c"]
+for Check in a:
+    b = str(a).replace(Check[0+i], Change[0+i])
+    i+1
+
+print(b)
